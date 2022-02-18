43386170e5610509c491a91c1a1baa73388f7df5
QSP <QSP@QSP>
2022-02-18 09:08:47 +0530

    hi

---
+++ src/test/java/testscripts/Tstcase5.java
@@ -0,0 +1,12 @@
+package testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tstcase5 {
+	@Test
+	public void tc5() {
+		Reporter.log("hello postman",true);
+	}
+
+}
---
+++ src/test/java/testscripts/Tstcase6.java
@@ -0,0 +1,12 @@
+package testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tstcase6 {
+	@Test
+	public void tc6() {
+		Reporter.log("hello API",true);
+	}
+
+}
